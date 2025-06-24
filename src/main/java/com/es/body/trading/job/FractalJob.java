318678c0e4534505ca12b21719a8649264bcffc9
AlexHabba <alexhabba112@gmail.com>
2025-06-24 14:13:46 +0300

    - up version v2

--- src/main/java/com/es/body/trading/job/FractalJob.java
+++ src/main/java/com/es/body/trading/job/FractalJob.java
@@ -11,6 +11,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Comparator.comparing;
@@ -23,15 +24,53 @@ public class FractalJob {
     private final BinanceSymbolsFetcher binanceSymbolsFetcher;
     private final FractalRepository fractalRepository;
 
-    @Scheduled(cron = "0 0 * * * *")
+//    @Scheduled(fixedDelay = 1000)
+    public void execute() {
+        int count = 27;
+        List<String> symbols = binanceSymbolsFetcher.getAllSymbol("USDT");
+        symbols.forEach(s -> {
+            List<Candle> candlesH1 = CandleApi.getCandleWithoutTicks(s, LocalDateTime.now(), "1H", 1000).stream()
+                    .sorted(comparing(Candle::getCreateDate))
+                    .collect(toList());
+
+            extracted(s, candlesH1, count, "1h");
+
+            List<Candle> candlesH4 = CandleApi.getCandleWithoutTicks(s, LocalDateTime.now(), "4h", 1000).stream()
+                    .sorted(comparing(Candle::getCreateDate))
+                    .collect(toList());
+
+            extracted(s, candlesH4, count, "4h");
+
+            List<Candle> candlesD1 = CandleApi.getCandleWithoutTicks(s, LocalDateTime.now(), "1d", 1000).stream()
+                    .sorted(comparing(Candle::getCreateDate))
+                    .collect(toList());
+
+            extracted(s, candlesD1, count, "1d");
+
+        });
+
+        System.out.println();
+    }
+
+    private void extracted(String s, List<Candle> candles, int count, String interval) {
+        for (int i = 0; i <= candles.size() - count; i++) {
+            // Берем подсписок из 27 свечей (окно)
+            List<Candle> window = candles.subList(i, i + count);
+
+            // Обрабатываем окно
+            getAndSaveFractal(s, interval, new ArrayList<>(window), count);
+        }
+    }
+
+        @Scheduled(cron = "0 0 * * * *")
     public void executeOneHour() {
         int count = 27;
         List<String> symbols = binanceSymbolsFetcher.getAllSymbol("USDT");
 
-        symbols.forEach(symbol -> this.getAndSaveFractal(symbol, "1H", count));
+        symbols.forEach(symbol -> this.getAndSaveFractal(symbol, "1h", count));
     }
 
-    @Scheduled(cron = "0 0 */4 * * *")
+        @Scheduled(cron = "0 0 */4 * * *")
     public void executeFourHour() {
         int count = 27;
         List<String> symbols = binanceSymbolsFetcher.getAllSymbol("USDT");
@@ -128,5 +167,40 @@ public class FractalJob {
             }
         }
         return true;
+    }
+
+    @SneakyThrows
+    private void getAndSaveFractal(String symbol, String interval, List<Candle> candles, int count) {
+        // чтобы сформировать фрактал, нужно слева и справа иметь одинаковое кол-во баров
+        // фрактал 13 баров берем кол-во баров умножаем на 2 и плюс 2
+
+        if (candles.size() != 27) {
+            return;
+        }
+
+        int median = candles.size() / 2;
+        Candle Candle = candles.get(median);
+
+        if (isLowFractalLeft(candles, median) && isLowFractalRight(candles, median)) {
+            Fractal fractal = Fractal.builder()
+                    .countCandle(count)
+                    .symbol(symbol)
+                    .createDate(Candle.getCreateDate())
+                    .interval(interval)
+                    .low(Double.toString(Candle.getLow()))
+                    .build();
+            saveFractal(fractal);
+        }
+
+        if (isHighFractalLeft(candles, median) && isHighFractalRight(candles, median)) {
+            Fractal fractal = Fractal.builder()
+                    .countCandle(count)
+                    .symbol(symbol)
+                    .createDate(Candle.getCreateDate())
+                    .interval(interval)
+                    .high(Double.toString(Candle.getHigh()))
+                    .build();
+            saveFractal(fractal);
+        }
     }
 }
