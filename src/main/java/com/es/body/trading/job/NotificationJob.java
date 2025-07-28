02fa96850f770f5a1fb935a69e24d8726ad2fcac
AlexHabba <alexhabba112@gmail.com>
2025-07-28 05:10:54 +0300

    - add notif

---
+++ src/main/java/com/es/body/trading/job/NotificationJob.java
@@ -0,0 +1,50 @@
+package com.es.body.trading.job;
+
+import com.bybit.api.client.config.BybitApiConfig;
+import com.bybit.api.client.domain.account.AccountType;
+import com.bybit.api.client.domain.account.request.AccountDataRequest;
+import com.bybit.api.client.service.BybitApiClientFactory;
+import com.es.body.service.SenderService;
+import com.es.body.trading.CandleService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationJob {
+
+    private final ObjectMapper objectMapper;
+    private final SenderService senderService;
+
+    @Scheduled(cron = "13 * */3 * * *")
+    public void executeJob() {
+        showPositionAndBalance();
+    }
+
+    @SneakyThrows
+    private void showPositionAndBalance() {
+        BigDecimal balance = getBalance("Bm93uykPRKyNZqaGeI", "NLrdAqquHmoCjxXU3ynmx6f4XypEq5gOufMe");
+        senderService.send(1466178855L, "Сейчас на счете " + balance);
+    }
+
+    @SneakyThrows
+    public BigDecimal getBalance(String key, String secret) {
+        var client = BybitApiClientFactory.newInstance(key, secret, BybitApiConfig.MAINNET_DOMAIN, true).newAccountRestClient();
+        AccountDataRequest request = AccountDataRequest.builder().accountType(AccountType.UNIFIED).coin("USDT").build();
+        Object walletBalance = client.getWalletBalance(request);
+        String string = objectMapper.writeValueAsString(walletBalance);
+        WalletBalanceDto wallet = objectMapper.readValue(string, WalletBalanceDto.class);
+        return wallet.getResult().getList().get(0).getTotalEquity();
+    }
+
+    public static void main(String[] args) {
+        NotificationJob notificationJob = new NotificationJob(new ObjectMapper(), null);
+        notificationJob.showPositionAndBalance();
+    }
+}
